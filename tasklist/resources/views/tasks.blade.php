c9c9cf650a447c049e79c89beb7df857e875a808
Mateus Freitas <matthelink.rm.rf@gmail.com>
2021-12-13 16:42:43 -0300

    Dev Updates

--- tasklist/resources/views/tasks.blade.php
+++ tasklist/resources/views/tasks.blade.php
@@ -34,14 +34,30 @@
                                         </th>
                                         @if (!$task->done)
                                             <th class="p-4">
-                                                <a class="btn btn-success" href="{{ route('change', $task->id) }}">Completar</a>
-                                                <a class="btn btn-info" href="{{ route('editTaskView', $task->id) }}">Editar</a>
-                                                <a class="btn btn-error" href="{{ route('delete', $task->id) }}">Deletar</a>
+                                                <a class="btn btn-success btn-circle btn-outline" href="{{ route('change', $task->id) }}">
+                                                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                                                        <path fill="currentColor" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" />
+                                                    </svg>
+                                                </a>
+                                                <a class="btn btn-info btn-circle btn-outline" href="{{ route('editTaskView', $task->id) }}">
+                                                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                                                        <path fill="currentColor" d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
+                                                    </svg>
+                                                </a>
+                                                <a class="btn btn-error btn-circle btn-outline" href="{{ route('delete', $task->id) }}">
+                                                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                                                        <path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+                                                    </svg>
+                                                </a>
                                             </th>
                                         @else
-                                            <th>
+                                            <th class="flex align-center">
                                                 Objetivo Completo!
-                                                <a class="btn btn-error btn-outline p-4" href="{{ route('delete', $task->id) }}">Deletar</a>
+                                                <a class="btn btn-error btn-circle btn-outline" href="{{ route('delete', $task->id) }}">
+                                                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                                                        <path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+                                                    </svg>
+                                                </a>
                                             </th>
                                         @endif
                                     </tr>
